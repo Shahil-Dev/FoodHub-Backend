b8993382261ef71e701211bda060bcf196d92769
Shahil-Dev <alfee9006@gmail.com>
2026-02-22 03:07:12 +0600

    3rd commit

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { postRouter } from "./modules/post.router";
+import { AuthRoutes } from "./modules/Auth/auth.route";
 
 const app = express();
 app.use(express.json());
 
 
 app.use("/posts", postRouter.router)
-
+app.use("/api/v1/auth", AuthRoutes)
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
---
+++ src/modules/Auth/auth.controller.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import { AuthService } from "./auth.service";
+
+const createUser = async (req: Request, res: Response) => {
+    try {
+        const result = await AuthService.createUser(req.body);
+
+        res.cookie("token", result.token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "strict"
+        });
+
+        res.status(200).json({
+            success: true,
+            message: "User created successfully",
+            data: result.user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error instanceof Error ? error.message : "Unknown error"
+        });
+    }
+};
+
+const loginUser = async (req: Request, res: Response) => {
+    try {
+        const result = await AuthService.loginUser(req.body);
+
+        res.cookie("token", result.token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "strict"
+        });
+
+        res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            data: result.user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error instanceof Error ? error.message : "Unknown error"
+        });
+    }
+};
+
+export const AuthController = {
+    createUser,
+    loginUser
+};
