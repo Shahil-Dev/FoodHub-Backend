1bc907820d029d704d93670dd3290f798851d09b
Shahil-Dev <alfee9006@gmail.com>
2026-03-15 01:38:03 +0600

    fix: cors and endpoint

--- src/App.ts
+++ src/App.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"]
