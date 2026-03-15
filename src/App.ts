2042df3a0e46c0b9a4a4da781492ac3f3307e44f
Shahil-Dev <alfee9006@gmail.com>
2026-03-15 14:08:01 +0600

    configure environment variables and database connection

--- src/App.ts
+++ src/App.ts
@@ -8,11 +8,13 @@ const app: Application = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: true,
+    origin: ["http://localhost:3000"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], 
-    allowedHeaders: ["Content-Type", "Authorization"]
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
 }));
+
+app.options("/", cors());
 
 app.use("/api/v1", router);
 app.use("/posts", postRouter.router);
