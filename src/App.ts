9ac1c9d19f80e207ddea1c5515d6d98ac2bacff1
Shahil-Dev <alfee9006@gmail.com>
2026-07-19 22:55:17 +0600

    update backend

--- src/App.ts
+++ src/App.ts
@@ -1,35 +1,47 @@
 import express, { Application, Request, Response } from "express";
+
 import cors from "cors";
+
 import { postRouter } from "./modules/post.router";
+
 import router from "./routes";
+
 import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
+
 app.use(
-  app.use(
-    cors({
-      origin: [
-        "http://localhost:3000",
-        "https://assignment-4-frontend-b6q4.vercel.app",
-      ],
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-      allowedHeaders: [
-        "Content-Type",
-        "Authorization",
-        "X-Requested-With",
-        "Accept",
-      ],
-    }),
-  ),
+  cors({
+    origin: [
+      "http://localhost:3000",
+
+      "https://assignment-4-frontend-b6q4.vercel.app/",
+    ],
+
+    credentials: true,
+
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+
+    allowedHeaders: [
+      "Content-Type",
+
+      "Authorization",
+
+      "X-Requested-With",
+
+      "Accept",
+    ],
+  }),
 );
 
 app.options("/", cors());
 
 app.use("/api/v1", router);
+
 app.use("/posts", postRouter.router);
 
 app.get("/", (req: Request, res: Response) => {
